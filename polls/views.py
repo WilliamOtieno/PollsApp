01ee338e8d3c62894a4552407a6743a213de10c8
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-04-22 12:01:51 +0300

    Modified results.html

--- polls/views.py
+++ polls/views.py
@@ -21,8 +21,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of the question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
